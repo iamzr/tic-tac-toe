c49d64eb61ad294413aecc56091ecfe0bc23cc00
Zia Rehman <iamziarehman@gmail.com>
2021-08-25 22:41:21 +0100

    Added eventListeners for each square

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
-const gameBoard = (function (p1, p2) {
+const gameBoard = (function () {
+    console.log("gameBoard on")
     board = new Array(9);
 
     const Player = (name, symbol) => {
-        return {name, symbol}
+        let positions = []
+        return {name, symbol, positions}
     };
 
-    player1 = Player(p1);
-    player2 = Player(p2);
-      
-    return {board}
+    player1 = Player("p1", "x");
+    player2 = Player("p2", "o");
+    
+    whosTurn = false; // False = player 1, true = player 2
+    function turn (position){
+        if (!whosTurn) {
+            player1.positions = position;
+        } else {
+            player2.positions = position;
+        }
+        whosTurn = !whosTurn;
+        updateWhosTurn(whosTurn);
+    }
+
+    return {board, whosTurn}
 }) ()
 
 const displayController = (() => {
+    console.log("display controller on")
 
-    const squares = document.querySelectorAll("square")
-        squares.forEach(square => {
-            square.addEventListener("click", () =>{
-                console.log(square.id)
-                return square.id;
+    console.log("running")
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(square => {
+        square.addEventListener("click", () => {
+            console.log(square.id)
+        })
+    })
+
+    function playerTurn(square) {
+        console.log(square.id);
+    }
+
+   function updateWhosTurn (bool) {
+       let turn = "";
+
+       if (!bool) {
+           turn = "Player 1"
+       } else {
+           turn = "Player 2"
+       }
+
+       whosTurnDiv = document.getElementById("whos-turn");
+       whosTurnDiv.textContent = turn;
+    
+   }
+
+   return {playerTurn, updateWhosTurn}
  
-})
+}) ()
