2e7ed1ef8c4461d99d32527dcef3d2119eb70eae
Zia Rehman <iamziarehman@gmail.com>
2021-08-27 03:54:11 +0100

    Can now end the game using winningCombos

--- script.js
+++ script.js
@@ -3,19 +3,11 @@ const gameBoard = (function () {
     let board = new Array(9);
 
     const Player = (name, symbol) => {
-        let positions = [5];
+        let positions = [];
         return {name, symbol, positions}
     };
 
-    winningCombos = [
-            [1,2,3],
-            [4,5,6],
-            [7,8,9],
-            [1,4,7],
-            [2,5,8],
-            [3,6,9],
-            [1,5,9],
-            [3,5,7]];
+
 
     let player1 = Player("p1", "x");
     let player2 = Player("p2", "o");
@@ -36,9 +28,11 @@ const gameBoard = (function () {
             if (!whosTurn) {
                 player1.positions.push(selectedPosition)
                 displayController.updateBoard(player1, selectedPosition)
+                anyWins(player1)
             } else {
                 player2.positions.push(selectedPosition)
                 displayController.updateBoard(player2, selectedPosition)
+                anyWins(player2)
             }
             whosTurn = !whosTurn;
         }
@@ -49,6 +43,26 @@ const gameBoard = (function () {
             return false;
         } else {
             return true;
+        }
+    }
+
+    function anyWins(player) {
+        let winningCombos = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+            [1,4,7],
+            [2,5,8],
+            [3,6,9],
+            [1,5,9],
+            [3,5,7]];
+        console.log(player.positions) 
+        for (let i=0; i<winningCombos.length; i++){
+            let check = winningCombos[i].every(val => player.positions.includes(val.toString()));
+            console.log(check)
+            if (check) {
+                return displayController.gameOver(player)
+            }
         }
     }
 
@@ -72,5 +86,19 @@ const displayController = (() => {
         square.textContent = player.symbol;
     }
 
-    return {updateBoard}
+    function gameOver() {
+        document.getElementById("game-display").setAttribute("style", "display:none")
+        document.getElementById("game-over").classList = ""
+    }
+
+    function restart() {
+        gameBoard.player1.positions = []
+        gameBoard.player2.positions = []
+        document.getElementById("game-display").setAttribute("style", "display:block")
+    }
+
+    playAgain = document.getElementById("play-again")
+    playAgain.addEventListener("click", restart())
+
+    return {updateBoard, gameOver}
 }) ()
